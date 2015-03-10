a999c4c416926ee395c528f4f10ad3b3ac24ced0
Richard Schulte <richard.j.schulte@gmail.com>
2015-03-10 14:32:00 -0400

    added controller references, no controllers yet

--- src/app/index.js
+++ src/app/index.js
@@ -18,48 +18,57 @@ angular.module('gtkpressCalculator', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSa
       .state('steps', {
         abstract: true,
         templateURL: 'app/steps/steps.html',
+        controller: 'StepsCtrl',
         url: '/steps'
       })
       .state('steps.basics', {
         // loaded into ui-view of parent's template
         templateUrl: 'app/steps/basics/steps.bascics.html',
+        controller: 'StepsBasicCtrl',
         url: '/basics'
       })
       .state('steps.style', {
         // loaded into ui-view of parent's template
         templateUrl: 'app/steps/style/steps.style.html',
+        controller: 'StepsStyleCtrl',
         url: '/style'
+      })
+      .state('steps.binding', {
+        // loaded into ui-view of parent's template
+        templateUrl: 'app/steps/binding/steps.binding.html',
+        controller: 'StepsBindingCtrl',
+        url: '/binding'
+      })
+      .state('steps.paper-type', {
+        // loaded into ui-view of parent's template
+        templateUrl: 'app/steps/paper-type/steps.paper-type.html',
+        controller: 'StepsPaperTypeCtrl',
+        url: '/paper-type'
+      })
+      .state('steps.cover', {
+        // loaded into ui-view of parent's template
+        templateUrl: 'app/steps/cover/steps.cover.html',
+        controller: 'StepsCoverCtrl',
+        url: '/cover'
+      })
+      .state('steps.shipping', {
+        // loaded into ui-view of parent's template
+        templateUrl: 'app/steps/shipping/steps.shipping.html',
+        controller: 'StepsShippingCtrl',
+        url: '/shipping'
+      })
+      .state('steps.quote', {
+        // loaded into ui-view of parent's template
+        templateUrl: 'app/steps/quote/steps.quote.html',
+        controller: 'StepsQuoteCtrl',
+        url: '/quote'
+      })
+      .state('steps.inquire', {
+        // loaded into ui-view of parent's template
+        templateUrl: 'app/steps/inquire/steps.inquire.html',
+        controller: 'StepsInquireCtrl',
+        url: '/inquire'
       });
-      // .state('steps.binding', {
-      //   // loaded into ui-view of parent's template
-      //   templateUrl: 'app/steps/binding/steps.binding.html',
-      //   url: '/binding'
-      // })
-      // .state('steps.paper-type', {
-      //   // loaded into ui-view of parent's template
-      //   templateUrl: 'app/steps/paper-type/steps.paper-type.html',
-      //   url: '/paper-type'
-      // })
-      // .state('steps.cover', {
-      //   // loaded into ui-view of parent's template
-      //   templateUrl: 'app/steps/cover/steps.cover.html',
-      //   url: '/cover'
-      // })
-      // .state('steps.shipping', {
-      //   // loaded into ui-view of parent's template
-      //   templateUrl: 'app/steps/shipping/steps.shipping.html',
-      //   url: '/shipping'
-      // })
-      // .state('steps.quote', {
-      //   // loaded into ui-view of parent's template
-      //   templateUrl: 'app/steps/quote/steps.quote.html',
-      //   url: '/quote'
-      // })
-      // .state('steps.inquire', {
-      //   // loaded into ui-view of parent's template
-      //   templateUrl: 'app/steps/inquire/steps.inquire.html',
-      //   url: '/inquire'
-      // });
 
     $urlRouterProvider.otherwise('/');
   })
